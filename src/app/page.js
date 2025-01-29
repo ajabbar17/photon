d6ba80081fafd9d44d0ccb8fdbf2f76bccf7bd48
ajabbar17 <ajabbar1234@gmail.com>
2025-01-30 03:14:49 +0500

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 
 import Image from "next/image";
-import Grid from "./components/Grid";
 import Carousel from "./components/Carousel";
 import HeroSection from "./components/HeroSection";
 import Welcome from "./components/Welcome";
 import MarqueeSection from "./components/Marquee";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/Navbar";
-import Alt1 from "./components/Alt1";
-import Alt2 from "./components/Alt2";
+
 
 
 export default function Home() {
@@ -17,12 +15,9 @@ export default function Home() {
       <Navbar />
       <HeroSection />
      <Welcome />
-      <Carousel />
      <MarqueeSection />
      <Footer />
-     {/* <Alt1 /> */}
-     <Alt2 />
-    {/* <Grid /> */}
+   
     </div>
   );
 }
