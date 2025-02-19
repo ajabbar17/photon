b24f41932685643839229b68ef90a28abe43ffb6
ajabbar17 <ajabbar1234@gmail.com>
2025-02-20 03:23:11 +0500

    change in footer

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import Circle2 from './Circle2';
 const Footer = () => {
   return (
     <div className="bg-[#0d2538] relative h-screen overflow-hidden z-0">
-        <div className='wrapper h-[82%] md:h-[77%]  xl:h-[80%] z-50'>
+        <div className='wrapper h-[85%] md:h-[77%]  xl:h-[80%] z-50'>
       <div className="footer bg-[#0d2538] grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3 gap-5 px-8 md:px-16 py-3 md:py-12 z-20">
         {/* Column 1: Logo */}
         <div className="flex flex-col items-start">
