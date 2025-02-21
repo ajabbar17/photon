e5fb6f250249b84eed62298d622ea86e4cb34163
ajabbar17 <ajabbar1234@gmail.com>
2025-02-21 21:13:42 +0500

    added input field and logo redirection

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,113 +1,153 @@
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Circle from './Circle';
 import Circle2 from './Circle2';
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    if (!email) {
+      console.log("Email is required");
+      return;
+    }
+
+    const objectToSubmit = {
+      email,
+      access_key: "bb1403d8-9a4e-4edd-8e59-2cb2714b41d9",
+    };
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(objectToSubmit),
+      });
+      const result = await response.json();
+      if (result.success) {
+        console.log(result);
+        setEmail(''); // Clear the input field after successful submission
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  }
+
   return (
     <div className="bg-[#0d2538] relative h-screen overflow-hidden z-0">
-        <div className='wrapper h-[85%] md:h-[77%]  xl:h-[80%] z-50'>
-      <div className="footer bg-[#0d2538] grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3 gap-5 px-8 md:px-16 py-3 md:py-12 z-20">
-        {/* Column 1: Logo */}
-        <div className="flex flex-col items-start">
-          <div className="mb-4">
-            <Image
-              src="/Group 142.svg" // Replace with the actual logo URL
-              alt="Logo"
-              className="w-36 "
-              width={128}
-              height={128}
-            />
+      <div className='wrapper h-[88%] md:h-[77%]  xl:h-[80%] z-50'>
+        <div className="footer bg-[#0d2538] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 px-8 md:px-16 py-3 md:py-12 z-20">
+          {/* Column 1: Logo */}
+          <div className="flex flex-col items-start">
+            <div className="mb-4">
+              <Link href="/">
+                <Image
+                  src="/Group 142.svg"
+                  alt="Logo"
+                  className="w-32"
+                  width={128}
+                  height={128}
+                />
+              </Link>
+            </div>
+          </div>
+
+          {/* Column 2: Quick Links and Company */}
+          <div className="text-white grid grid-cols-2 gap-5 xl:gap-10">
+            <div>
+              <h3 className="text-lg font-normal heading mb-2 md:mb-4">SERVICES</h3>
+              <ul className="space-y-2 text-xs md:text-sm detail text-gray-400">
+                <li><Link href="/services/BMS" className="hover:text-white">BMS</Link></li>
+                <li><Link href="/services/elevators" className="hover:text-white">Elevators, Escalators & Moving Walkways</Link></li>
+                <li><Link href="/services/fire" className="hover:text-white">Fire Detection, Alarm & Protection System</Link></li>
+                <li><Link href="/services/HVAC" className="hover:text-white">Heating Ventilation and Air Conditioning Solutions</Link></li>
+                <li><Link href="/services/lightning" className="hover:text-white">Lightning Protection System</Link></li>
+                <li><Link href="/services/drainage" className="hover:text-white">Plumbing: Water Supply & Drainage Solutions</Link></li>
+                <li><Link href="/services/solarpowered" className="hover:text-white">Solar Powered System Solutions</Link></li>
+                <li><Link href="/services/wind" className="hover:text-white">Wind Energy Solutions</Link></li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-lg  heading font-normal  mb-2 md:mb-4">QUICK LINKS</h3>
+              <ul className="space-y-2 text-xs md:text-sm text-gray-400">
+                <li><Link href="/contact" className="hover:text-white">Contact Us</Link></li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Column 3: Email Subscription and Social Media Icons */}
+          <div className="text-white w-full">
+            {/* Email Subscription */}
+            <div className="w-full">
+              <form onSubmit={handleSubmit} className="flex w-full">
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="ENTER YOUR GMAIL"
+                  className="bg-[#0a2540] border w-[200px] md:w-[230px] xl:w-[294px] border-[#F25525] placeholder:font-semibold placeholder:text-white text-white px-4 py-2 rounded-l-[99px] outline-none"
+                />
+                <button 
+                  type="submit"
+                  className="bg-orange-500 uppercase font-bold w-auto xl:w-[160px] -ml-7 border-2 text-white px-6 xl:px-[47px] py-1 xl:py-2 rounded-[99px] hover:bg-orange-600"
+                >
+                  Join Us
+                </button>
+              </form>
+            </div>
+
+            {/* Social Media Icons */}
+            <div className="flex mb-20 mt-4 space-x-4">
+              <Link href="https://www.instagram.com/photonengineeringrealm?igsh=ZjZ1NWVtd3Ywd2U2" target='_blank' className="text-white text-xl hover:text-gray-300">
+                <i className="fab fa-instagram">
+                  <Image 
+                    src="/ig.png"
+                    alt="Instagram"
+                    width={32}
+                    height={32}
+                  />
+                </i>
+              </Link>
+              <Link href="https://www.linkedin.com/company/photon-engineering-realm/" target='_blank' className="text-white text-xl hover:text-gray-300">
+                <i className="fab fa-linkedin">
+                  <Image 
+                    src="/in.png"
+                    alt="LinkedIn"
+                    width={32}
+                    height={32}
+                  />
+                </i>
+              </Link>
+              <Link href="https://www.facebook.com/share/14vj9njXZR/?mibextid=wwXIfr" target='_blank' className="text-white text-xl hover:text-gray-300">
+                <i className="fab fa-facebook">
+                  <Image 
+                    src="/fb.png"
+                    alt="facebook"
+                    width={32}
+                    height={32}
+                  />
+                </i>
+              </Link>
+            </div>
           </div>
         </div>
-
-        {/* Column 2: Quick Links and Company */}
-        <div className="text-white grid grid-cols-2 gap-5 xl:gap-10">
-          <div>
-            <h3 className="text-lg font-normal heading mb-2 md:mb-4">SERVICES</h3>
-            <ul className="space-y-2 text-xs md:text-sm detail text-gray-400">
-              <li><Link href="/services/BMS" className="hover:text-white">BMS</Link></li>
-              <li><Link href="/services/elevators" className="hover:text-white">Elevators, Escalators & Moving Walkways</Link></li>
-              <li><Link href="/services/fire" className="hover:text-white">Fire Detection, Alarm & Protection System</Link></li>
-              <li><Link href="/services/HVAC" className="hover:text-white">Heating Ventilation and Air Conditioning Solutions</Link></li>
-              <li><Link href="/services/lightning" className="hover:text-white">Lightning Protection System</Link></li>
-              <li><Link href="/services/drainage" className="hover:text-white">Plumbing: Water Supply & Drainage Solutions</Link></li>
-              <li><Link href="/services/solarpowered" className="hover:text-white">Solar Powered System Solutions</Link></li>
-              <li><Link href="/services/wind" className="hover:text-white">Wind Energy Solutions</Link></li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="text-lg  heading font-normal  mb-2 md:mb-4">QUICK LINKS</h3>
-            <ul className="space-y-2 text-xs md:text-sm text-gray-400">
-              <li><Link href="/contact" className="hover:text-white">Contact Us</Link></li>
-            </ul>
-          </div>
-        </div>
-
-        {/* Column 3: Email Subscription and Social Media Icons */}
-        <div className="text-white w-full ">
-          {/* Email Subscription */}
-          <div className="  w-full ">
-          <h3 className="text-lg  heading font-normal mb-4">SOCIAL LINKS</h3>
-
-            {/* <div className="flex w-full">
-              <input
-                type="email"
-                placeholder="ENTER YOUR GMAIL"
-                className="bg-[#0a2540] border w-[200px] md:w-[230px] xl:w-[294px] border-[#F25525] placeholder:font-semibold placeholder:text-white text-white px-4 py-2  rounded-l-[99px] outline-none"
-              />
-              <button className="bg-orange-500 uppercase font-bold w-auto  xl:w-[160px] -ml-7 border-2 text-white px-6 xl:px-[47px] py-1 xl:py-2 rounded-[99px] hover:bg-orange-600">
-                Join Us
-              </button>
-            </div> */}
-          </div>
-
-          {/* Social Media Icons */}
-          <div className="flex mb-20 space-x-4">
-            <Link href="https://www.instagram.com/photonengineeringrealm?igsh=ZjZ1NWVtd3Ywd2U2" target='_blank' className="text-white text-xl hover:text-gray-300">
-              <i className="fab fa-instagram">
-                <Image 
-                  src="/ig.png"
-                  alt="Instagram"
-                  width={32}
-                  height={32}
-                />
-              </i> {/* Replace with icon */}
-            </Link>
-            <Link href="https://www.linkedin.com/company/photon-engineering-realm/" target='_blank' className="text-white text-xl hover:text-gray-300">
-              <i className="fab fa-linkedin">
-                <Image 
-                  src="/in.png"
-                  alt="LinkedIn"
-                  width={32}
-                  height={32}
-                />
-              </i>
-            </Link>
-            <Link href="https://www.facebook.com/share/14vj9njXZR/?mibextid=wwXIfr" target='_blank' className="text-white text-xl  hover:text-gray-300">
-              <i className="fab fa-facebook">
-                <Image 
-                  src="/fb.png"
-                  alt="facebook"
-                  width={32}
-                  height={32}
-                />
-              </i>
-            </Link>
-          </div>
-        </div>
-      </div>
       </div>
       <Circle bottom={0} right={35} />
-        <Circle bottom={10} right={48} />
-        <Circle bottom={10} right={30} />
-        <Circle bottom={15} right={20} />
-        <Circle bottom={20} right={15} />
-        <Circle bottom={18} right={10} />
-        <Circle bottom={18} right={0} />
-        <Circle2 bottom={17} right={30} />
-        <Circle2 bottom={20} right={35} />
+      <Circle bottom={10} right={48} />
+      <Circle bottom={10} right={30} />
+      <Circle bottom={15} right={20} />
+      <Circle bottom={20} right={15} />
+      <Circle bottom={18} right={10} />
+      <Circle bottom={18} right={0} />
+      <Circle2 bottom={17} right={30} />
+      <Circle2 bottom={20} right={35} />
     </div>
   );
 };
--- src/app/components/services/Footer.jsx
+++ src/app/components/services/Footer.jsx
@@ -1,14 +1,49 @@
 "use client"
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Circle from "../footer/Circle";
 import Link from "next/link";
 import Circle2 from "../footer/Circle2";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
   const handleEmailClick = () => {
     window.location.href = "/contact-form";
   };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    if (!email) {
+      console.log("Email is required");
+      return;
+    }
+
+    const objectToSubmit = {
+      email,
+      access_key: "bb1403d8-9a4e-4edd-8e59-2cb2714b41d9",
+    };
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(objectToSubmit),
+      });
+      const result = await response.json();
+      if (result.success) {
+        console.log(result);
+        setEmail(''); // Clear the input field after successful submission
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  }
+
   return (
     <div className="bg-[#0d2538] relative h-screen  z-0">
       <div className="wrapper h-[82%] md:h-[77%]  xl:h-[80%]  z-50">
@@ -16,13 +51,15 @@ const Footer = () => {
           {/* Column 1: Logo */}
           <div className="flex flex-col text-white items-start max-w-[500px]">
             <div className="mb-4">
-              <Image
-                src="/Group 142.svg" // Replace with the actual logo URL
-                alt="Logo"
-                className="w-32"
-                width={128}
-                height={128}
-              />
+              <Link href="/">
+                <Image
+                  src="/Group 142.svg"
+                  alt="Logo"
+                  className="w-32"
+                  width={128}
+                  height={128}
+                />
+              </Link>
             </div>
             <h1 className="detail font-semibold ">Contact us at</h1>
             <p className="detail font-extralight">
@@ -32,32 +69,34 @@ const Footer = () => {
               your dream project.
             </p>
             <button 
-            onClick={handleEmailClick}
-            className="mt-8 px-4 py-3 w-[156px] source h-[47px] bg-[#f25525] text-white text-base font-normal rounded-3xl hover:bg-[#C6370C] transition">
-            Get Started Today
-          </button>
+              onClick={handleEmailClick}
+              className="mt-8 px-4 py-3 w-[156px] source h-[47px] bg-[#f25525] text-white text-base font-normal rounded-3xl hover:bg-[#C6370C] transition">
+              Get Started Today
+            </button>
           </div>
 
           {/* Column 2: Email Subscription and Social Media Icons */}
-          <div className="text-white w-full flex flex-col items-start md:items-center ">
+          <div className="text-white w-full flex flex-col items-start md:items-center">
             {/* Email Subscription */}
-            <div className=" w-full flex ">
-              {/* <div className="flex">
+            <div className="w-full flex xl:justify-end">
+              <form onSubmit={handleSubmit} className="flex">
                 <input
                   type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   placeholder="ENTER YOUR GMAIL"
-                  className="bg-[#0a2540] border w-[200px] xl:w-[294px] border-[#F25525] placeholder:font-semibold placeholder:text-white text-white px-4 py-2  rounded-l-[99px] outline-none"
+                  className="bg-[#0a2540] border w-[200px] xl:w-[294px] border-[#F25525] placeholder:font-semibold placeholder:text-white text-white px-4 py-2 rounded-l-[99px] outline-none"
                 />
-                <button className="bg-orange-500 uppercase font-bold w-[160px] -ml-7 border-2 text-white px-9 md:px-[47px] py-2 rounded-[99px] hover:bg-orange-600">
+                <button 
+                  type="submit"
+                  className="bg-orange-500 uppercase font-bold w-[160px] -ml-7 border-2 text-white px-9 md:px-[47px] py-2 rounded-[99px] hover:bg-orange-600">
                   Join Us
                 </button>
-              </div> */}
+              </form>
             </div>
-            <h3 className="text-lg  heading font-normal mb-4">SOCIAL LINKS</h3>
 
             {/* Social Media Icons */}
-
-            <div className="flex  mb-20  space-x-4">
+            <div className="flex mb-20 mt-4 space-x-4">
               <Link href="https://www.instagram.com/photonengineeringrealm?igsh=ZjZ1NWVtd3Ywd2U2" target="_blank" className="text-white text-xl hover:text-gray-300">
                 <i className="fab fa-instagram">
                   <Image src="/ig.png" alt="Instagram" width={32} height={32} />
@@ -68,7 +107,7 @@ const Footer = () => {
                   <Image src="/in.png" alt="LinkedIn" width={32} height={32} />
                 </i>
               </Link>
-              <Link href="https://www.facebook.com/share/14vj9njXZR/?mibextid=wwXIfr" target="_blank" className="text-white text-xl  hover:text-gray-300">
+              <Link href="https://www.facebook.com/share/14vj9njXZR/?mibextid=wwXIfr" target="_blank" className="text-white text-xl hover:text-gray-300">
                 <i className="fab fa-facebook">
                   <Image src="/fb.png" alt="facebook" width={32} height={32} />
                 </i>
@@ -86,7 +125,7 @@ const Footer = () => {
       <Circle bottom={21} right={10} />
       <Circle bottom={21} right={0} />
       <Circle2 bottom={17} right={30} />
-        <Circle2 bottom={20} right={35} />
+      <Circle2 bottom={20} right={35} />
     </div>
   );
 };
