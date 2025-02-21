f61e10fc7e823810a4ca85f802d5547716676ec1
ajabbar17 <ajabbar1234@gmail.com>
2025-02-21 20:08:19 +0500

    page.js image

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-
-import Carousel from "./components/Carousel";
 import HeroSection from "./components/HeroSection";
 import Welcome from "./components/Welcome";
 import MarqueeSection from "./components/Marquee";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/Navbar";
-import ServiceCarousel from "./components/Alt3";
-import Alt1 from "./components/Alt1";
 import Image from "next/image";
 import Alt4 from "./components/Alt4";
 
@@ -17,7 +13,7 @@ export default function Home() {
     <div className="bg-[#0d2538] relative">
       <Navbar />
       <Image src="/heroimg.png"
-          width={1920}
+          width={2560}
           height={1080}
           className='absolute top-0  z-40 left-0 '
         alt="hero" />
